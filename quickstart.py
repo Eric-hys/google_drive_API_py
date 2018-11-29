25bf350b0d20ae0a078322588fdbf211e916e29f
eric-hys <eric.hys64@gmail.com>
2018-11-29 06:31:40 +0000

    update

--- quickstart.py
+++ quickstart.py
@@ -39,21 +39,7 @@ def main():
     """Shows basic usage of the Drive v3 API.
     Prints the names and ids of the first 10 files the user has access to.
     """
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    # store = file.Storage('token.json')
-    # creds = store.get()
-    # if not creds or creds.invalid:
-    #     flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
-    #     creds = tools.run_flow(flow, store)
-    # service = build('drive', 'v3', http=creds.authorize(Http()))
-    #
-    # # Call the Drive v3 API
-    # results = service.files().list(
-    #     pageSize=100, fields="nextPageToken, files(id, name)", q="'1RqrUDMy9M07R8db496bVNnRqlB3YqgLj' in parents and trashed = false").execute()
-    # items = results.get('files', [])
-
+    
     google_drive = Gdrive()
 
     items = google_drive.get_file('1RqrUDMy9M07R8db496bVNnRqlB3YqgLj')
@@ -63,9 +49,6 @@ def main():
     else:
         print('Files:')
         for item in items:
-            # school_dir = service.files().list(
-            #     pageSize=100, fields="nextPageToken, files(id, name)", q="'" + item['id'] + "' in parents and trashed = false").execute()
-            # doc_list = school_dir.get('files', [])
             model_list = google_drive.get_file(item['id'])
 
             if not model_list:
